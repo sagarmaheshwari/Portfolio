7950700d517071f7ef09dc6f6008c2e5ec1f7f3f
sagarmaheshwari <maheshwarisagar2001@gmail.com>
2023-07-08 23:49:20 +0530

    docs

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MobileComponent } from './mobile/mobile.component';
+import { AboutComponent } from './about/about.component';
 
 
 
 @NgModule({
   declarations: [
-    MobileComponent
+    MobileComponent,
+    AboutComponent
   ],
   imports: [
     CommonModule,
   ],
   exports: [
-    MobileComponent
+    MobileComponent,
+    AboutComponent
   ]
 })
 export class HomeModule { }
