377a92d7cd44802dcc1a289078a91bc01a72cbc8
sagarmaheshwari <maheshwarisagar2001@gmail.com>
2023-07-08 16:53:59 +0530

    Mobile-screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portfolio';
+  innerWidth?: any;
+  innerHeight?: any;
+
+  ngOnInit() {
+    this.innerHeight = window.innerHeight;
+    this.innerWidth = window.innerWidth;
+    console.log(this.innerHeight, this.innerWidth)
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+  this.innerWidth = window.innerWidth;
+  this.innerHeight = window.innerHeight;
+}
 }
